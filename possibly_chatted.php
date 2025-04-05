604e3e78524eb93c401c0262d8372905a017323c
inkoson007 <89128565+inkoson007@users.noreply.github.com>
2025-04-05 17:34:35 +0300

    Обновление V0.6
    Добавили страницу с кем возможно общался
    Изменили ссылку в списке друзей и подписчиков на страницу в профиле этого сайта
    Добавили ссылку на страницу в вк в профиле
    Добавили количество подарков в профиль

--- possibly_chatted.php
+++ possibly_chatted.php
@@ -55,6 +55,7 @@ foreach ($friends as $friend) {
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Возможные собеседники</title>
     <link rel="stylesheet" href="styles.css">
 </head>
